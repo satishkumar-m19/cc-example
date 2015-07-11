37a5d1fd57c3b72c09fbf7f728cf3898ed5bab71
Satishkumar Masilamani <satishkumar.m19@gmail.com>
2015-07-10 23:34:32 -0500

    Completed

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,1 +1,50 @@
 # example of program that calculates the median number of unique words per tweet.
+#!/usr/bin/python
+
+# Program which provides the running median, here we have used the concept of heaps.
+# maintain the first half of the stream in max heap and the 2nd half in the min heap
+# based on the count of the heap elements return the appropriate elements.
+# Author: Satishkumar Masilamani
+# Created: July 10, 2015
+
+import heapq as hq
+import random
+
+
+def running_median(first_half_max_heap, second_half_min_heap, current_item):
+    diff = len(first_half_max_heap) - len(second_half_min_heap)
+    if diff == 0:
+        if second_half_min_heap[0] <= current_item:
+            hq.heappush(second_half_min_heap, current_item)
+            return float(second_half_min_heap[0])
+        else:
+            hq.heappush(first_half_max_heap, -current_item)
+            return float(-first_half_max_heap[0])
+    elif diff > 0:
+        if second_half_min_heap[0] <= current_item:
+            hq.heappush(second_half_min_heap, current_item)
+        else:
+            temp_pop = hq.heappushpop(first_half_max_heap, -current_item)
+            hq.heappush(second_half_min_heap, -temp_pop)
+        return float((second_half_min_heap[0] - first_half_max_heap[0])/2)
+    else:
+        if second_half_min_heap[0] <= current_item:
+            temp_pop = hq.heappushpop(second_half_min_heap, current_item)
+            hq.heappush(first_half_max_heap, -temp_pop)
+        else:
+            hq.heappush(first_half_max_heap, -current_item)
+        return float((second_half_min_heap[0] - first_half_max_heap[0])/2)
+
+
+def main():
+    list_of_numbers = [random.randint(0, 100) for i in range(100)]
+    first_half_max_heap = []
+    second_half_min_heap = []
+    hq.heappush(second_half_min_heap, 25)
+    print 'running median for the list of random 100 numbers : ', list_of_numbers
+    print 25
+    for each in list_of_numbers:
+        print running_median(first_half_max_heap, second_half_min_heap, each)
+
+if __name__ == '__main__':
+    main()
--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -1,1 +1,62 @@
 # example of program that calculates the total number of times each word has been tweeted.
+#!/usr/bin/python
+
+# Program which counts the number of words occured in the stream of tweets.
+# Author: Satishkumar Masilamani
+# Created: July 10, 2015
+
+from pandas import Series
+from collections import Counter
+import median_unique
+import os
+import sys
+
+
+def write_count_file(wordcount_series, count_filepath):
+    '''Function to write the word count'''
+    f_write = open(count_filepath, 'w')
+    f_write.write(wordcount_series.to_string())
+    f_write.close()
+
+
+def write_median_file(stream_of_median, median_filepath):
+    '''Function to write the Median file'''
+    f_write = open(median_filepath, 'w')
+    for each in stream_of_median:
+        f_write.write(repr(each) + '\n')
+    f_write.close()
+
+
+def main(tweets_filepath, count_filepath, median_filepath):
+    ''' Function to read the file and construct word counter '''
+    f_read = open(tweets_filepath, 'r')
+    wordcount_series = Series()
+    stream_of_median = []
+    first_half_max_heap = []
+    second_half_min_heap = []
+    first_element_median_flag = False
+    for each in f_read:
+        word_counter = Counter(each.lower().rstrip().split(' '))
+        word_series = Series(word_counter)
+        wordcount_series = wordcount_series.add(word_series, fill_value=0)
+        if first_element_median_flag:
+            curr_median = median_unique.running_median(first_half_max_heap, second_half_min_heap, float(len(word_counter.keys())))
+            stream_of_median.append(curr_median)
+        else:
+            first_element_median_flag = True
+            curr_median = len(word_counter.keys())
+            second_half_min_heap.append(curr_median)
+            stream_of_median.append(curr_median)
+    f_read.close()
+    write_count_file(wordcount_series, count_filepath)
+    write_median_file(stream_of_median, median_filepath)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        tweets_filepath = os.path.abspath(sys.argv[1])
+        count_filepath = os.path.abspath(sys.argv[2])
+        median_filepath = os.path.abspath(sys.argv[3])
+        main(tweets_filepath, count_filepath, median_filepath)
+    else:
+        main()
